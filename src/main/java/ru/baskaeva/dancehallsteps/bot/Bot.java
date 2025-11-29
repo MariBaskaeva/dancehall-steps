8247ca49ca56661f595a9a59b663c42d7a6539cb
MariBaskaeva <mariab1@list.ru>
2025-11-29 18:30:18 +0300

    @fix DHS-17 исправила проблему с командой /start

--- src/main/java/ru/baskaeva/dancehallsteps/bot/Bot.java
+++ src/main/java/ru/baskaeva/dancehallsteps/bot/Bot.java
@@ -54,6 +54,7 @@ public class Bot extends TelegramLongPollingBot {
         }
 
         if(start){
+            start = false;
             var path = "static/start";
             sendText(user.getId(), MyFileReader.readFile(path), true);
         }
